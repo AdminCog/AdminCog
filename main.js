91c12d26f018946add368d26a4acc48947847444
AdminCog <acog2025@gmail.com>
2024-09-25 19:27:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,10 @@ function showModal(modalId) {
 function closeModal(modalId) {
     document.getElementById(modalId).style.display = 'none';
 }
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    hamburger.classList.toggle('toggle');
+});
